bb32bb593eba48d1093c8e190db9fc8986dafbe9
Ricardo Lorenzi <rlorenzi@gmail.com>
2022-06-21 19:48:21 -0300

    Subir desafio Operador Ternario

--- js/signUpValidation.js
+++ js/signUpValidation.js
@@ -1,19 +1,14 @@
 function formValidation() {
-    uid = document.formSignUp.userid;
+    var uid = document.formSignUp.userid;
+    var pswid = document.formSignUp.pswid;
     if (useridValidation(uid, 5, 12)) {}
     return false;
 }
 
 function useridValidation(uid, min, max) {
-    uidSize = uid.value.length;
-    if (uidSize == 0 || uidSize >= max || uidSize < min) {
-        Swal.fire('El Usuario no puede estar Vacio y debe ser entre: ' + min + ' y ' + max);
-        uid.focus();
-        return false;
-    }
-    document.getElementById('usuario');
-    usuario = document.getElementById('usuario').value;
-    sessionStorage.setItem('Usuario', usuario);
-    return true;
-
-};
+    var userid = document.formSignUp.userid.value;
+    var uidSize = userid.length;
+    (uidSize == 0 || uidSize >= max || uidSize < min) ?
+    Swal.fire('El Usuario no puede estar Vacio y debe ser entre: ' + min + ' y ' + max):
+        sessionStorage.setItem('Usuario', userid);
+}
