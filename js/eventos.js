8d4040fa973a257717b20ee82be59e4887771091
Ricardo Lorenzi <rlorenzi@gmail.com>
2022-06-23 20:13:01 -0300

    Subir desafio sobre librerias

--- js/eventos.js
+++ js/eventos.js
@@ -1,9 +1,24 @@
-function aceptarEvento(evento) {
-    log.textContent = `Gracias por contactarnos: ${nombre.value}`;
-    event.preventDefault();
-}
-
 const form = document.getElementById('formContact');
 const nombre = document.getElementById('nameContact');
 const log = document.getElementById('mensaje');
+// const fecha = DateTime.local(2022, 1, 25, 12, 10);
+// const fecha = DateTime();
+// const now = DateTime.now();
+// const fecha = DateTime.now();
+// const dt = DateTime.now();
+ const fecha = luxon.DateTime.now();
+ const dia = fecha.day;
+ const mes = fecha.month;
+ const ano = fecha.year;
+ const fechaCompleta = dia + '/' + mes + '/' + ano;
+
+// const DateTime = luxon.DateTime;
+// const fecha = dt.toLocaleString(DateTime.DATE_FULL);
 form.addEventListener('submit', aceptarEvento);
+
+function aceptarEvento(evento) {
+    log.textContent = `Gracias por contactarnos: ${nombre.value} ${fechaCompleta}`;
+    // log.textContent = `Gracias por contactarnos: ${nombre.value} + ' ' + ${Fecha.value}`;
+    console.log(fechaCompleta);
+    event.preventDefault();
+}
--- js/signUpValidation.js
+++ js/signUpValidation.js
@@ -1,7 +1,9 @@
 function formValidation() {
     var uid = document.formSignUp.userid;
-    var pswid = document.formSignUp.pswid;
-    if (useridValidation(uid, 5, 12)) {}
+    var passid = document.formSignUp.pswid;
+    if (useridValidation(uid, 5, 12)) {
+        if (passid_validation(passid, 7, 12)) {}
+    }
     return false;
 }
 
@@ -11,4 +13,12 @@ function useridValidation(uid, min, max) {
     (uidSize == 0 || uidSize >= max || uidSize < min) ?
     Swal.fire('El Usuario no puede estar Vacio y debe ser entre: ' + min + ' y ' + max):
         sessionStorage.setItem('Usuario', userid);
+}
+
+function passid_validation(passid, min, max) {
+    var passid = document.formSignUp.pswid.value;
+    var pswSize = document.formSignUp.pswid.value.length;
+    (pswSize == 0 || pswSize >= max || pswSize < min) ?
+    Swal.fire('El Password no puede estar Vacio y debe ser entre: ' + min + ' y ' + max):
+        sessionStorage.setItem('Password', passid);
 }
