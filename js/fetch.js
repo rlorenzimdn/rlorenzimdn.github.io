afe8be79669bf7ef0b90c85a4a7fc8f8f1bc1990
Ricardo Lorenzi <rlorenzi@gmail.com>
2022-06-29 11:50:57 -0300

    Subir Desafio Fetch

--- js/fetch.js
+++ js/fetch.js
@@ -1,10 +1,10 @@
 fetch("../books.json")
   .then((resp) => resp.json())
   .then((data) => {
-    const lista = document.querySelector("div");
+    const bookList = document.querySelector("div");
     data.forEach((post) => {
       const ul = document.createElement("ul");
-      ul.innerHTML = `<h4>${post.name}</h4><p>${post.price}</p>`;
-      lista.append(ul);
+      ul.innerHTML = `<h4>${post.name}</h4><p>`+'Precio: '+`${post.price}</p><p>`+'Disponible: '+`${post.stock}</p>`;
+      bookList.append(ul);
     });
   });
