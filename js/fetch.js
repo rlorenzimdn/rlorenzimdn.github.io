9dac8de176d129ec2f565a85bdbe3925d2a8ef6d
Ricardo Lorenzi <rlorenzi@gmail.com>
2022-07-04 13:35:35 -0300

    Subir ajustes varios

--- js/fetch.js
+++ js/fetch.js
@@ -1,10 +1,11 @@
-fetch("../books.json")
+fetch("../json/books.json")
   .then((resp) => resp.json())
   .then((data) => {
-    const bookList = document.querySelector("div");
-    data.forEach((post) => {
-      const ul = document.createElement("ul");
-      ul.innerHTML = `<h4>${post.name}</h4><p>`+'Precio: '+`${post.price}</p><p>`+'Disponible: '+`${post.stock}</p>`;
-      bookList.append(ul);
-    });
+    let cad = "<tr><th>Nombre del Libro</th><th>Precio en Pesos</th><th>Disponibles</th></tr>";
+    for (let usuario of data) {
+      cad += `<tr><td>${usuario.name}</td>
+                <td>${usuario.price}</td>
+                <td>${usuario.stock}</td></tr>`;
+    }
+    document.getElementById("tabla1").innerHTML = cad;
   });
